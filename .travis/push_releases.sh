7db0203c37acaeb1a8f61e39eacc6d5c025914d0
Joseph Lust <joe@longreen.io>
2018-01-06 22:07:43 -0500

    Update deploy keys

--- .travis/push_releases.sh
+++ .travis/push_releases.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-# TODO move repo location to build ENV var
-git remote add origin https://${GH_ACCESS_TOKEN}@github.com/twisedpair/google-cloud-sdk.git
-git push --quiet --set-upstream origin master 
+git push --quiet origin master --tags
+echo "git sync complete"
---
+++ .travis/setup_git_keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Decrypt and install GH deploy key
+KEY_NAME=travis_deploy_key.private
+openssl aes-256-cbc -k "$GITHUB_KEY_PASSWORD" -d -a -in "$KEY_NAME.enc" -out $KEY_NAME
+KEY_FILE_PATH=`pwd`/$KEY_NAME
+echo -e "Host github.com\n  IdentityFile $KEY_FILE_PATH" > ~/.ssh/config
+
+# Preaccept GH's key
+echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
