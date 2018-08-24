8c89b371ddb5440ff912fae354811fae8527c803
Joseph Lust <twistedpair@users.noreply.github.com>
2018-08-24 16:31:14 -0400

    Another try

--- .travis/setup_git_keys.sh
+++ .travis/setup_git_keys.sh
@@ -18,4 +18,5 @@ echo "Installing github.com in known_hosts..."
 echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
 
 # Ensure we can authentica
+ssh-agent sh -c "ssh-add ${KEY_FILE_PATH}; ssh -T git@github.com"
 ssh -T git@github.com
