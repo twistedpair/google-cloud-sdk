c18534d050a8b220b0428163df3fc3100d3072f8
Joseph Lust <lifeoflust@gmail.com>
2018-08-25 10:49:05 -0400

    Debugging

--- .travis/push_results_github.sh
+++ .travis/push_results_github.sh
@@ -4,5 +4,9 @@ set -e
 cd  /tmp/gcloud_history/                                                                                                                                      
 git tag -l
 git remote add origin https://${GH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
-git push -u origin master --tags                                                                                                                              
+
+git pull origin master --verbose
+
+git push -u origin master --tags --verbose                                                                                                                              
+git log --graph --oneline --decorate --all 
 echo "git sync complete"                                                                                                                                      
