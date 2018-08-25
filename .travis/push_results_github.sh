8fc2b8dea8d0c33611ef6dbbcf82f3f30288c827
Joseph Lust <lifeoflust@gmail.com>
2018-08-25 10:31:23 -0400

    Use GH token for push

--- .travis/push_results_github.sh
+++ .travis/push_results_github.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-set -e
-
-cd  /tmp/gcloud_history/
-ssh-agent sh -c "ssh-add ${KEY_FILE_PATH}; git push -u origin master --tags"
-echo "git sync complete"
+#!/bin/bash                                                                                                                                                   
+set -e                                                                                                                                                        
+                                                                                                                                                              
+cd  /tmp/gcloud_history/                                                                                                                                      
+git remote add origin https://${GH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
+git push -u origin master --tags                                                                                                                              
+echo "git sync complete"                                                                                                                                      
