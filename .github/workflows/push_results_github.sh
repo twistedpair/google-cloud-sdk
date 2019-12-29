bb772d8fc89e400be3a85b18d01296befa13f735
Joseph Lust <lifeoflust@gmail.com>
2019-12-29 16:08:25 -0500

    Enable debugging

--- .github/workflows/push_results_github.sh
+++ .github/workflows/push_results_github.sh
@@ -1,13 +1,15 @@
-#!/bin/bash                                                                                                                                                   
-set -e                                                                                                                                                        
+#!/bin/bash
+set -eu
 
 echo "Switch to checkout path"
-cd  /tmp/gcloud_history/                                                                                                                                
+cd  /tmp/gcloud_history/
 
 echo "Setup push token"
-git remote add origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
+git remote add origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                               
+
 echo "Push tags and commits..."
 git pull origin master --verbose
-git push -u origin master --tags --verbose                                                                                                                              
-git log --graph --oneline --decorate --all 
-echo "git sync complete"                                                                                                                                      
+git push -u origin master --tags --verbose
+
+git log --graph --oneline --decorate --all
+echo "git sync complete"
