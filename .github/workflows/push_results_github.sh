69f976cab3733b0dfa5d975850c28340ce2ebaa1
Joseph Lust <lifeoflust@gmail.com>
2019-12-29 16:14:57 -0500

    Fix remotes

--- .github/workflows/push_results_github.sh
+++ .github/workflows/push_results_github.sh
@@ -4,11 +4,8 @@ set -eu
 echo "Switch to checkout path"
 cd  /tmp/gcloud_history/
 
-echo "Check remotes"
-git remote -v
-
 echo "Setup push token"
-git remote add origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git
+git remote set-url origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git
 
 echo "Push tags and commits..."
 git pull origin master --verbose
