8072fedf7ffc742bd17b9a146790a16302873735
Joseph Lust <lifeoflust@gmail.com>
2019-12-29 16:12:53 -0500

    Debug error messages

--- .github/workflows/push_results_github.sh
+++ .github/workflows/push_results_github.sh
@@ -8,7 +8,7 @@ echo "Check remotes"
 git remote -v
 
 echo "Setup push token"
-git remote add origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                               
+git remote add origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git
 
 echo "Push tags and commits..."
 git pull origin master --verbose
