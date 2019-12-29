71837eaa9dad22e2162906734fef2ee59165512e
Joseph Lust <lifeoflust@gmail.com>
2019-12-29 16:03:50 -0500

    Setup token

--- .github/workflows/push_results_github.sh
+++ .github/workflows/push_results_github.sh
@@ -1,10 +1,11 @@
 #!/bin/bash                                                                                                                                                   
 set -e                                                                                                                                                        
-                                                                                                                                                              
-cd  /tmp/gcloud_history/                                                                                                                                      
 
-git remote add origin https://${GH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
+echo "Switch to checkout path"
+cd  /tmp/gcloud_history/                                                                                                                                
 
+echo "Setup push token"
+git remote add origin https://${GITHUB_PUSH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
 echo "Push tags and commits..."
 git pull origin master --verbose
 git push -u origin master --tags --verbose                                                                                                                              
