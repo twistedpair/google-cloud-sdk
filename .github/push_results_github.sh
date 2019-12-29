8c914a0516d42768b33f6043cdeb7392c1fff406
Joseph Lust <lifeoflust@gmail.com>
2019-12-29 15:40:40 -0500

    Add GitHub workflow

--- .github/push_results_github.sh
+++ .github/push_results_github.sh
@@ -2,11 +2,9 @@
 set -e                                                                                                                                                        
                                                                                                                                                               
 cd  /tmp/gcloud_history/                                                                                                                                      
-git tag -l
-git remote add origin https://${GH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
 
+echo "Push tags and commits..."
 git pull origin master --verbose
-
 git push -u origin master --tags --verbose                                                                                                                              
 git log --graph --oneline --decorate --all 
 echo "git sync complete"                                                                                                                                      
