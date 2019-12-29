1348271b972eee235978f2510cd03d6650715699
Joseph Lust <lifeoflust@gmail.com>
2019-12-29 15:56:50 -0500

    Wireup secrets

--- .github/push_results_github.sh
+++ .github/push_results_github.sh
@@ -3,6 +3,8 @@ set -e
                                                                                                                                                               
 cd  /tmp/gcloud_history/                                                                                                                                      
 
+git remote add origin https://${GH_TOKEN}@github.com/twistedpair/google-cloud-sdk.git > /dev/null 2>&1                                                        
+
 echo "Push tags and commits..."
 git pull origin master --verbose
 git push -u origin master --tags --verbose                                                                                                                              
